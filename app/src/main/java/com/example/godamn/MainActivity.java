af9ee5e134560c0a656a3017e5a03ae90386b4df
unknown <dani.stamm.pessoa.oliveira@gmail.com>
2025-08-11 09:09:43 -0300

    Contador

--- app/src/main/java/com/example/godamn/MainActivity.java
+++ app/src/main/java/com/example/godamn/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.godamn;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    int numClick = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textView = findViewById(R.id.tv_contador);
+
+        textView.setText(Integer.toString(numClick));
+        textView.setOnClickListener(v -> {
+
+            numClick++;
+            textView.setText(Integer.toString(numClick));
+
+        });
+
+
     }
 }
