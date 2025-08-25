1a60efb2da8eecdd10b3d348279acaf81680b470
unknown <daniel stamm pessoa de oliveira>
2025-08-25 08:33:04 -0300

    ciclos de vida

--- app/src/main/java/com/example/godamn/MainActivity.java
+++ app/src/main/java/com/example/godamn/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.godamn;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,44 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("cicloVida", "onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("cicloVida", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("cicloVida", "onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("cicloVida", "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("cicloVida", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("cicloVida", "onStop");
     }
 }
+
