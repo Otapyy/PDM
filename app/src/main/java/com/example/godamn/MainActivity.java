13a9db4fd0b8bcc702a609275f82a3af1ec0c3c7
Daniel Stamm Pessoa de Oliveira <dani.stamm.pessoa.oliveira@gmail.com>
2025-10-02 16:14:09 -0300

    entrega

--- app/src/main/java/com/example/godamn/MainActivity.java
+++ app/src/main/java/com/example/godamn/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.godamn;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+            EditText campoNome = findViewById(R.id.campoNome);
+            EditText campoPeso = findViewById(R.id.campoPeso);
+            EditText campoAltura = findViewById(R.id.campoAltura);
+
+            Button btnDados = findViewById(R.id.btnDados);
+
+
+            btnDados.setOnClickListener(v ->{
+
+            String dadoTratarNome = campoNome.getText().toString();
+            String dadoTratarPeso = campoPeso.getText().toString();
+            String dadoTratarAltura = campoAltura.getText().toString();
+
+            double dadoPeso = Double.parseDouble(dadoTratarPeso);
+            double dadoAltura = Double.parseDouble(dadoTratarAltura);
+
+            if(!dadoTratarAltura.isEmpty() && !dadoTratarPeso.isEmpty() && !dadoTratarNome.isEmpty()){
+                Intent intent = new Intent(this, ResultadoActivity.class);
+                intent.putExtra("nome", dadoTratarNome);
+                intent.putExtra("peso", dadoPeso);
+                intent.putExtra("altura", dadoAltura);
+                startActivity(intent);
+            }
+
+            });
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/godamn/ResultadoActivity.java
+++ app/src/main/java/com/example/godamn/ResultadoActivity.java
@@ -1,6 +1,7 @@
 package com.example.godamn;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +14,49 @@ public class ResultadoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_resultado);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView campoNomeResultado = findViewById(R.id.campoNomeResultado);
+        TextView campoImcResultado = findViewById(R.id.campoImcResultado);
+        TextView campoResultado = findViewById(R.id.campoResultadoTexto);
+
+        double intentPeso = getIntent().getDoubleExtra("peso", 1);
+        double intentAltura = getIntent().getDoubleExtra("altura", 1);
+        String intentNomeResultado = getIntent().getStringExtra("nome");
+        double imcResultado = intentPeso / (intentAltura * intentAltura);
+        String textImcResultado = "Seu IMC é: " + imcResultado + "!";
+
+        campoNomeResultado.setText(intentNomeResultado);
+        campoImcResultado.setText(textImcResultado);
+
+        //nao consegui fazer as imagens a tempo ;((((
+
+        if(imcResultado < 16){
+            campoResultado.setText("Magreza grave!");
+
+        } else if (16 <= imcResultado && imcResultado < 17) {
+            campoResultado.setText("magreza moderada");
+
+        } else if (17 <= imcResultado && imcResultado < 18.5) {
+            campoResultado.setText("magreza leve");
+
+        } else if (18.5 <= imcResultado && imcResultado < 25) {
+            campoResultado.setText("saudavel");
+
+        } else if (25 <= imcResultado && imcResultado < 30) {
+            campoResultado.setText("sobrepeso");
+
+        } else if (30 <= imcResultado && imcResultado < 35) {
+            campoResultado.setText("obesidade grau 1");
+
+        } else if (35 <= imcResultado && imcResultado < 40) {
+            campoResultado.setText("obesidade grau 2");
+
+        } else if (imcResultado > 40) {
+            campoResultado.setText("obesidade grau 3");
+
+        }
+
+
     }
 }
