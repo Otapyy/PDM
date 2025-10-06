272ae4e3c7f69770ff0d2b793ff3a26f3efd38f7
NatVansuita <natanjoaovansuita@gmail.com>
2025-10-06 08:59:46 -0300

    a

--- app/src/main/java/com/example/godamn/MainActivity.java
+++ app/src/main/java/com/example/godamn/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.godamn;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -13,28 +15,52 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
-    String[] nomes = new String[] {"Daniel", "Henrique", "Anami"};
+    ArrayList<String> nomes;
     ListView listView;
+    Button btnSalvar;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         listView = findViewById(R.id.listView);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        editText = findViewById(R.id.etNome);
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+        nomes = new ArrayList<>(Arrays.asList("Daniel", "Henrique", "Anami"));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nomes);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            Toast.makeText(getApplicationContext(), nomes.get(position), Toast.LENGTH_LONG).show();
         });
 
-        ArrayAdapter<String> adapter =  new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, android.R.id.text1,
-                nomes);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            Toast.makeText(getApplicationContext(), nomes[position], Toast.LENGTH_LONG).show();
+        listView.setOnItemLongClickListener((parent, view, position, id) -> {
+            Toast.makeText(getApplicationContext(), nomes.get(position), Toast.LENGTH_LONG).show();
+            nomes.remove(position);
+            adapter.notifyDataSetChanged();
+            return true;
         });
+
+        btnSalvar.setOnClickListener(v -> {
+                nomes.add(editText.getText().toString());
+                adapter.notifyDataSetChanged();
+
+        });
+
+
+
     }
 }
